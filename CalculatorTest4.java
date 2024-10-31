c83d1a0f57b98030b0a8d06aa7f6a1a6aa31ce5a
Suryansh Rai <151069189+suryanshrai1@users.noreply.github.com>
2024-10-31 23:46:16 +0530

    Added Materials

---
+++ CalculatorTest4.java
@@ -0,0 +1,19 @@
+public class CalculatorTest4 {
+    public static void main(String[] args) {
+        
+        Calculator4 calc = new Calculator4();
+        
+        //Name your friends
+        calc.findTotal(10, null);
+        calc.findTotal(12, null);
+        calc.findTotal(9,  null);
+        calc.findTotal(8,  null);
+        calc.findTotal(7,  null);
+        calc.findTotal(15, "Alex");
+        calc.findTotal(11, null);
+        calc.findTotal(30, "Forgetful");
+
+        //Find and print the entire table's total, including tax and tip
+       
+    } 
+}
